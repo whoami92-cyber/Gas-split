5da15d7822ae8420e97331ce67b7699f45f14226
whoami92-cyber <niko.iijolainen@outlook.com>
2026-04-07 10:02:41 +0300

    gas split

---
+++ gash.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+
+app=Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def caculator():
+    f=None
+    if request.method=='POST':
+        b= float (request.form['travel'])
+        c=float (request.form['cars'])
+        e=float (request.form['gasprice'])
+        g=float (request.form['p'])
+        a=(b/100)*c
+        d=a*e
+        f=d/g
+        f= round(f, 2)
+    return render_template('index.html', total=f)
+if __name__=='__main__':
+    app.run(debug=True)
+
+
