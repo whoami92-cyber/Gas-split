3627ac970977275b046f1fae7022722732146e1d
Niko <niko.iijolainen@outlook.com>
2026-04-09 18:55:56 +0300

    Update gash.py

--- gash.py
+++ gash.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 
 app=Flask(__name__)
 @app.route('/')
-    return render_template('index.html')
+return render_template('index.html')
 @app.route('/robots.txt')
 def robots():
     return "User-agent: *\n Allow: /", 200, {'Content-Type: text/plain'}
